4da7d2a727472698cd1d8c2e85a1d019a4570cfc
ErhanTK <125463792+ErhanTK@users.noreply.github.com>
2023-12-08 02:00:22 +0000

    Update employee.py

--- employee.py
+++ employee.py
@@ -2,30 +2,50 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name):
+    def __init__(self, name, contract_type, salary=None, hourly_rate=None, hours_worked=None, bonus_commission=None, contract_commission=None, contracts_landed=None):
         self.name = name
+        self.contract_type = contract_type
+        self.salary = salary
+        self.hourly_rate = hourly_rate
+        self.hours_worked = hours_worked
+        self.bonus_commission = bonus_commission
+        self.contract_commission = contract_commission
+        self.contracts_landed = contracts_landed
 
     def get_pay(self):
-        pass
+        pay = 0
+        if self.contract_type == 'salary':
+            pay = self.salary
+        elif self.contract_type == 'hourly':
+            pay = self.hourly_rate * self.hours_worked
+        
+        if self.bonus_commission:
+            pay += self.bonus_commission
+        if self.contract_commission and self.contracts_landed:
+            pay += self.contract_commission * self.contracts_landed
+
+        return pay
 
     def __str__(self):
-        return self.name
+        description = f"{self.name} works on a "
+        if self.contract_type == 'salary':
+            description += f"monthly salary of {self.salary}"
+        elif self.contract_type == 'hourly':
+            description += f"contract of {self.hours_worked} hours at {self.hourly_rate}/hour"
 
+        if self.bonus_commission:
+            description += f" and receives a bonus commission of {self.bonus_commission}"
+        if self.contract_commission and self.contracts_landed:
+            description += f" and receives a commission for {self.contracts_landed} contract(s) at {self.contract_commission}/contract"
 
-# Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+        description += f". Their total pay is {self.get_pay()}."
+        return description
 
-# Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+# Employee objects creation
+billie = Employee('Billie', 'salary', salary=4000)
+charlie = Employee('Charlie', 'hourly', hourly_rate=25, hours_worked=100)
+renee = Employee('Renee', 'salary', salary=3000, contract_commission=200, contracts_landed=4)
+jan = Employee('Jan', 'hourly', hourly_rate=25, hours_worked=150, contract_commission=220, contracts_landed=3)
+robbie = Employee('Robbie', 'salary', salary=2000, bonus_commission=1500)
+ariel = Employee('Ariel', 'hourly', hourly_rate=30, hours_worked=120, bonus_commission=600)
 
-# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
-
-# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
-
-# Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
-
-# Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
